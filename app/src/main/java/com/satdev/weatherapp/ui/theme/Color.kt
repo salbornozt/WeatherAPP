df35b1c007523d65b9faa4f62f04928123c59e07
santiago <salbornozt@unbosque.edu.co>
2024-06-01 18:27:11 -0500

    init

---
+++ app/src/main/java/com/satdev/weatherapp/ui/theme/Color.kt
@@ -0,0 +1,27 @@
+package com.satdev.weatherapp.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+// Dark Theme Colors
+val TempleDarkPrimary = Color(0xFF1A5D57)
+val TempleDarkSecondary = Color(0xFF373737)
+val TempleDarkTertiary = Color(0xFF8D8D8D)
+val TempleDarkBackground = Color(0xFF000000)
+val TempleDarkSurface = Color(0xFF212121)
+val TempleDarkOnPrimary = Color(0xFFFFFFFF)
+val TempleDarkOnSecondary = Color(0xFFFFFFFF)
+val TempleDarkOnTertiary = Color(0xFF000000)
+val TempleDarkOnBackground = Color(0xFFFFFFFF)
+val TempleDarkOnSurface = Color(0xFFFFFFFF)
+
+// Light Theme Colors
+val TempleLightPrimary = Color(0xFF008D75)
+val TempleLightSecondary = Color(0xFFEEEEEE)
+val TempleLightTertiary = Color(0xFF616161)
+val TempleLightBackground = Color(0xFFFFFFFF)
+val TempleLightSurface = Color(0xFFFFFFFF)
+val TempleLightOnPrimary = Color(0xFF000000)
+val TempleLightOnSecondary = Color(0xFF000000)
+val TempleLightOnTertiary = Color(0xFFFFFFFF)
+val TempleLightOnBackground = Color(0xFF000000)
+val TempleLightOnSurface = Color(0xFF000000)
