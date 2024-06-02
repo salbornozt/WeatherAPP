b266dfc205b3e470f9361499773ef0a5ffaeef2c
salbornozt <salbornozt@unbosque.edu.co>
2024-06-02 11:55:34 -0500

    ui fixes

--- app/src/main/java/com/satdev/weatherapp/core/ui/BottomNavigationBar.kt
+++ app/src/main/java/com/satdev/weatherapp/core/ui/BottomNavigationBar.kt
@@ -1,15 +1,27 @@
 package com.satdev.weatherapp.core.ui
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.offset
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.material3.Divider
 import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.NavigationBar
 import androidx.compose.material3.NavigationBarItem
+import androidx.compose.material3.NavigationBarItemDefaults
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
 import com.satdev.weatherapp.navigation.ScreenDestinations
 
@@ -19,19 +31,33 @@ fun BottomNavigationBar(navController: NavController) {
         ScreenDestinations.Home,
         ScreenDestinations.Forecast,
     )
-    var selectedItem : ScreenDestinations by remember { mutableStateOf(ScreenDestinations.Home) }
+    var selectedItem: ScreenDestinations by remember { mutableStateOf(ScreenDestinations.Home) }
+    Column {
+        Divider(thickness = 1.dp, color = Color.Black)
+        NavigationBar(
+            containerColor = MaterialTheme.colorScheme.primary,
+            contentColor = MaterialTheme.colorScheme.onPrimary,
+        ) {
+            items.forEach { screen ->
+                NavigationBarItem(
+                    icon = {
+                        Icon(
+                            imageVector = screen.icon,
+                            contentDescription = stringResource(id = screen.resourceId),
+                            modifier = Modifier.size(45.dp)
+                        )
+                    },
+                    label = { Text(stringResource(id = screen.resourceId),style = MaterialTheme.typography.bodySmall.copy(fontSize = 12.sp), modifier = Modifier.offset(y = 10.dp)) },
+                    selected = selectedItem == screen,
+                    onClick = {
+                        selectedItem = screen
+                        navController.navigate(screen.route)
+                    },
+                    colors = NavigationBarItemDefaults.colors(indicatorColor = MaterialTheme.colorScheme.primary)
 
-    NavigationBar {
-        items.forEach { screen ->
-            NavigationBarItem(
-                icon = { Icon(imageVector = screen.icon, contentDescription = stringResource(id = screen.resourceId)) },
-                label = { Text(stringResource(id = screen.resourceId)) },
-                selected = selectedItem == screen,
-                onClick = {
-                    selectedItem = screen
-                    navController.navigate(screen.route)
-                },
-            )
+                )
+            }
         }
     }
+
 }
--- app/src/main/java/com/satdev/weatherapp/ui/theme/Type.kt
+++ app/src/main/java/com/satdev/weatherapp/ui/theme/Type.kt
@@ -17,34 +17,22 @@ val Typography = Typography(
     ),
     titleMedium = TextStyle(
         fontFamily = FontFamily.SansSerif,
-        fontWeight = FontWeight.Bold,
-        fontSize = 24.sp
+        fontWeight = FontWeight.Medium,
+        fontSize = 20.sp
     ),
-    bodySmall = TextStyle( // Wind, Feels like, High/Low
+    bodySmall = TextStyle(
         fontFamily = FontFamily.SansSerif,
         fontWeight = FontWeight.Normal,
-        fontSize = 16.sp
+        fontSize = 12.sp
     ),
-    titleSmall =  TextStyle( // Wind, Feels like, High/Low
+    bodyMedium = TextStyle(
+        fontFamily = FontFamily.SansSerif,
+        fontWeight = FontWeight.Medium,
+        fontSize = 14.sp
+    ),
+    titleSmall =  TextStyle(
         fontFamily = FontFamily.SansSerif,
         fontWeight = FontWeight.Medium,
         fontSize = 16.sp
     )
-
-    /* Other default text styles to override
-    titleLarge = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Normal,
-        fontSize = 22.sp,
-        lineHeight = 28.sp,
-        letterSpacing = 0.sp
-    ),
-    labelSmall = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Medium,
-        fontSize = 11.sp,
-        lineHeight = 16.sp,
-        letterSpacing = 0.5.sp
-    )
-    */
 )
